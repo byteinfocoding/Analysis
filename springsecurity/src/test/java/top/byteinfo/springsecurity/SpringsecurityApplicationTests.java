099ae2b5fa181131145b0c65445993ff4ef5b642
byteinfocoding <94441686+byteinfocoding@users.noreply.github.com>
2022-02-27 12:10:06 +0800

    initialize

---
+++ springsecurity/src/test/java/top/byteinfo/springsecurity/SpringsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package top.byteinfo.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
